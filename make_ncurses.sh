e7d547b3e2e68c6f44edcd3265b098af974fcd44
Nimrod Omer <nimrodomer@gmail.com>
2012-11-13 17:29:35 -0500

    Made it so that it compiles one binary (allows for header files, etc.).

--- make_ncurses.sh
+++ make_ncurses.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Compiles a C files passed in as arguments.
+# Compiles C files passed in as arguments.
 
-# Normally I would compile C files generically with make, but because -lncurses has to be the last argument to gcc, I gave up. I'm sure that there is a way to do it, and when I eventually take the time to understand make better, I will create the Makefile that does the below, and deprecate this script.
+# Normally I would compile C files generically with make. However, -lncurses has to be the last argument to gcc when compiling programs that use ncurses. This isn't easy to do in make, so I gave up on doing it this way for the time being. When I eventually take the time to understand make better, I will definitely create a Makefile that does the below, and deprecate this script.
+
+# Do note that this script doesn't do anything "fancy" like making sure that the arguments passed in are even C files, etc.
 
 ################################################################################
 # Functions
@@ -12,9 +14,11 @@ usage ()
 {
 	cat << EOF
 
-make_ncurses.sh source_1.c [source_2.c ... source_n-1.c source_n.c]
+make_ncurses.sh source_1.c [header_1.h source_2.c ... source_n-1.c header_n-1.h source_n.c header_n.h]
 
-(Where source_x.c is a valid C file.)
+(Where any source_x.[c|h] file must be a valid C source file.)
+
+N.B. source_1 will be the name of the binary output. This gives you the flexibility to compile one binary with a lot of source files, or a binary from a single source file if you're writing a quick and dirty single test program However, it will not allow you to compile several binaries at the same time.
 
 EOF
 	exit 1
@@ -25,24 +29,19 @@ compile ()
 	compiler='gcc'
 	compiler_flags='-g -Wall -Wextra'
 	linker_flags='-lncurses'
-	output="-o $( echo "${1}" | sed 's/\(.*\)\.c/\1/' )"
+	output="-o $( echo $1 | sed 's/\(.*\)\.c/\1/' )"
 
-	eval ${compiler} ${compiler_flags} ${output} ${1} ${linker_flags}
+	eval ${compiler} ${compiler_flags} ${output} "${@}" ${linker_flags}
 }
 
 ################################################################################
 # Main
 ################################################################################
 
-# If no C program files are specified in the argument list.
+# Checks to make sure that there is at least one argument.
 [ $# -lt 1 ] && usage
 
 # Compile all of the files in the argument list.
-x=1
-while [ $x -le $# ]
-do
-	compile ${!x}
-	x=$( expr $x + 1 )
-done
+compile "${@}"
 
 exit 0
